292726fb539fbc39dc32d1ed20459a32642620e6
John August <jpa52@drexel.edu>
2016-12-31 09:10:43 -0500

    Initial Commit
    FirstCommit

---
+++ RTI DataBase Updater V2/RTI.DataBase.Util/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RTI.DataBase.Util
+{
+    public class Logger
+    {
+        string _logPath;
+        DateTime _date = new DateTime();
+
+        /// <summary>
+        /// Initialize a new 
+        /// Logging session.
+        /// </summary>
+        public Logger()
+        {
+            _date = DateTime.UtcNow;
+            _logPath = Application.StartupPath;
+        }
+    }
+
+
+}
